2d078cfb8271f53a07e3c4bdd67af0a403602c41
Vlad Balamut <neighborhood999@gmail.com>
2025-06-17 23:38:59 +0500

    Adding two java files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // -A додати все що є в проєкті
+        // git rm --cached прописується задля того, щоб відмінити додавання файлу\-ів до commit
+        }
+    }
